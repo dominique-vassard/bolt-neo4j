885c7a0d98f919700b5eb016874f8696b8d16f9a
Dominique VASSARD <dominique.vassard@gmail.com>
2018-08-02 19:04:13 +0200

    Add test coverage tool

--- mix.exs
+++ mix.exs
@@ -8,7 +8,14 @@ defmodule BoltNeo4j.MixProject do
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -25,7 +32,8 @@ defmodule BoltNeo4j.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:calendar, "~> 0.17.2"}
+      {:calendar, "~> 0.17.2"},
+      {:excoveralls, "~> 0.8", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
