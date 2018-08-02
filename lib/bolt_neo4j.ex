ead7149c3ef770d9b9b094d0ec217685880e9d78
Dominique VASSARD <dominique.vassard@gmail.com>
2018-08-02 18:51:21 +0200

    Bug fix: add missing options

--- lib/bolt_neo4j.ex
+++ lib/bolt_neo4j.ex
@@ -13,8 +13,8 @@ defmodule BoltNeo4j do
     {:ok, port_} = :gen_tcp.connect(host, port, active: false, mode: :binary, packet: :raw)
 
     {:ok, _} = Bolt.handshake(:gen_tcp, port_, options)
-    {:ok, _info} = Bolt.init(:gen_tcp, port_, auth)
+    {:ok, _info} = Bolt.init(:gen_tcp, port_, auth, options)
 
-    Bolt.run_statement(:gen_tcp, port_, query, params)
+    Bolt.run_statement(:gen_tcp, port_, query, params, options)
   end
 end
